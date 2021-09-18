d0d68bf985a8ca59db036ea93e8a409b26529368
AlejandroWogelman <alehellamn@gmail.com>
2021-09-18 06:44:36 -0300

    .

--- src/Constates/const_info.js
+++ src/Constates/const_info.js
@@ -1,26 +1,23 @@
-
-
 const const_info = [
-    { general: {
-            
-            nombre: 'Alejandro',
-            ubicacion: 'Buenos Aires',
-            estudios: 'Cursos/Autodidacta',
-            orientacion_IT: 'Programacion Web'
-        }
-     },
-     {
-        estudios:{
-            
-            punchIt: ['Javascript 0-100', 'React 0-100'],
-            direccion: 'https://punchit.com.ar'
-        }
-     },
-     {
-         hobbies: ['Futbol', 'Series']
-        
-     }]
+  {
+    general: {
+      nombre: "Alejandro",
+      ubicacion: "Buenos Aires",
+      estudios: "Cursos/Autodidacta",
+      orientacion_IT: "Programacion Web",
+    },
+  },
+  {
+    estudios: {
+      punchIt: ["Javascript 0-100", "React 0-100"],
+      direccion: "https://punchit.com.ar",
 
+      Otros: "YouTube - Udemy - FreeCodeCamp",
+    },
+  },
+  {
+    hobbies: ["Futbol", "Series", "Anime"],
+  },
+];
 
- 
 export default const_info;
